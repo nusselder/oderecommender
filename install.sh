6b15e18fba2d9d47aa40e8f8bbff3bcf1e919066
Arjan Nusselder <github@nusselder.eu>
2014-06-25 16:52:57 +0200

    Removed some deprecated comments.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Log of steps taken to setup server.
+# Assumes no other running instances of node.js/redis.
 
 start_path=`pwd`
 
@@ -19,15 +20,10 @@ make
 cd $start_path
 ./programs/redis-2.8.12/src/redis-server 2>&1 >> redis.log &
 
-# Add node/bin to path, and set port environment (read during `npm start`)
+# Add node/bin to path, and set port environment (read during `npm start`/`forever start bin/www`).
 echo "export PATH=$PATH:$start_path/programs/node-v0.10.29-linux-x64/bin/" > load_node_path.sh
 echo "export PORT=8004" >> load_node_path.sh
 . load_node_path.sh
-
-# Get code from git.
-# git clone something.. # later, now just copy..
-#cp -r /scratch/users/anussel1/test-ode-recommender/oderecommender_clean/ oderecommender
-#cd oderecommender
 
 # Install dependencies.
 npm install -g forever
@@ -37,3 +33,5 @@ npm install
 #npm start
 forever start bin/www
 
+# To stop node.js `source load_node_path.sh` and the `forever stop all`.
+# To stop redis server `ps aux | grep redis` and kill the server process.
