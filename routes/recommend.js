312c010213e79f93d1dd8f3ae35b38a426d09a90
Arjan Nusselder <github@nusselder.eu>
2014-07-10 12:50:55 +0200

    implemented recommendation, TODO: format output

--- routes/recommend.js
+++ routes/recommend.js
@@ -19,7 +19,8 @@ router.get('/', function(req, res) {
   res.send( {'status': 'error', 'msg': 'Please specify a place_id.'} );
 });
 
-router.get('/:id', function(req, res) {
+
+router.get('/old/:id', function(req, res) {
 
   var recommendations = [];
 
@@ -34,5 +35,79 @@ router.get('/:id', function(req, res) {
   });
 
 });
+
+
+router.get('/:id', function(req, res) {
+
+  // The numbers of days to consider.
+  var days_back = 3;
+
+  // The current time/date
+  var timestamp = '2014-03-03T07:34:12Z';
+
+  // The place for which to get recommendations.
+  var place_id = req.params.id;
+
+  // ? keep track of rec.s here
+  var recommendations = [];
+
+  // ? log full request.
+  console.log('Request: '+place_id+' : '+timestamp+' : -'+days_back);
+
+  // Generate the list of dates.
+  var dates = [];
+  var today = new Date(timestamp);
+  for(var i=0; i<days_back; i++){
+    dates.push( today.toISOString().split('T')[0] );
+    today.setDate(today.getDate() - 1);
+  }
+
+  // Generate list of keys to retrieve the actual counts from.
+  // N.B. generating this list with not just the one given venue,
+  // but for each of a set of venues (i.e. all places a *user*
+  // visited) allows for personalised recommendation.
+  var coocurrence_keys = [];
+  var place_ids = [place_id]; // Example for future personalisation.
+  // var place_ids = [place_id, 'faux venue id'];
+  for(var i=0; i<dates.length; i++){
+    for(var j=0; j<place_ids.length; j++) {
+      coocurrence_keys.push( dates[i]+':'+place_ids[j] );
+    }
+  }
+
+  console.log('Dates: '+dates);
+  console.log('Co-occurence keys: '+coocurrence_keys);
+
+
+  // Get the list of all places relevant for the given timestamp and place_id.
+  req.redis_client.sunion(coocurrence_keys, function(err, timestamp_place_pairs){
+
+    console.log('Timestamped place-pairs: '+timestamp_place_pairs);
+
+    // Request, for each of these timestamp/place_pair combinations, the counts.
+    place_counts = {};
+    req.redis_client.hmget('timestamp:placepairs', timestamp_place_pairs, function(err, data){
+      for(var i=0; i < timestamp_place_pairs.length; i++) {
+        // Extract the recommended place_id from the key-string and combine it with its count-value.
+        // TODO: getting ids by splitting on keys is not pretty, and will go wrong if
+        // if the ids contain a ':'. We'll allow it in the first version.
+        var rec_place_id = timestamp_place_pairs[i].split(':')[2];
+        var count = parseInt(data[i]);
+        if (rec_place_id in place_counts ) place_counts[rec_place_id] += count;
+        else place_counts[rec_place_id] = count;
+
+      }
+
+      console.log(place_counts);
+      // todo, make place_counts now a sorted list..
+
+      res.json( {'status': 'accept', 'msg': 'Dummy recommendation', 'places': place_counts} );
+
+    });
+
+  });
+
+});
+
 
 module.exports = router;
--- routes/train.js
+++ routes/train.js
@@ -26,9 +26,87 @@ router.post('/', function(req, res) {
     // New, clean item.
     item = {"user_id":item.user_id,"place_id":item.place_id,"timestamp":item.timestamp};
 
+    // So, this is a My First Redis experience, note that:
+    // Sorted Sets is not necessary for this demo app.
+    // Similarly, pipelines (sounds like a good idea) are skipped.
+
+    // Also, note that visits are considered symmetrical (and is duplicated),
+    // e.g. there is no 'only here after there' notion in visits.
+    // Places are paired regardless of the time between visits, i.e.
+    // the "history" of a user only contains place_ids.
+
+
+    // item.timestamp == date only
+
+    // Update counts for the previously visited place_id's of the user.
+    req.redis_client.lrange('u:'+item.user_id, 0, 9, function(err, prev_places){
+
+      console.log('-'+item.user_id+'-: '+prev_places);
+      prev_places.forEach(function(prev_place_id){
+
+        // Old stuff
+        // Update the co-occurence of the two places, for the current day.
+        //var pair_1 = item.place_id+':'+place_id;
+        //var pair_2 = place_id+':'+item.place_id;
+
+        //req.redis_client.hincrby(item.timestamp, pair_1, 1, req.redis_print);
+        //req.redis_client.hincrby(item.timestamp, pair_2, 1, req.redis_print);
+
+        // Update a dictionary of all timestamp:place1:place2 triples, so we can get them in one go.
+        //req.redis_client.hincrby(item.timestamp, pair_1, 1, req.redis_print);
+
+        // Keep track of all pairs, for a given date and place.
+        //req.redis_client.sadd(item.timestamp+':'+item.place_id, pair_1, req.redis_print);
+        //req.redis_client.sadd(item.timestamp+':'+place_id, pair_2, req.redis_print);
+
+
+
+        // Biiig! hash tables, this is what redis is for right?
+
+        var timestamp_place = item.timestamp+':'+item.place_id;
+        var timestamp_place_pair = timestamp_place+':'+prev_place_id;
+        var reversed_timestamp_place = item.timestamp+':'+prev_place_id;
+        var reversed_timestamp_place_pair = reversed_timestamp_place+':'+item.place_id;
+
+        // Keep count of the co-occurence of places for each day.
+        req.redis_client.hincrby('timestamp:placepairs', timestamp_place_pair, 1);
+        req.redis_client.hincrby('timestamp:placepairs', reversed_timestamp_place_pair, 1);
+
+        // Keep a list of all co-occurences related to a specific date and place.
+        // (i.e. hash key management?)
+        req.redis_client.sadd(timestamp_place, timestamp_place_pair);
+        req.redis_client.sadd(reversed_timestamp_place, reversed_timestamp_place_pair);
+
+        // Also update an 'all' in case of too little data?
+        //req.redis_client.hincrby('t:all', pair_1, 1, req.redis_print);
+        //req.redis_client.hincrby('t:all', pair_2, 1, req.redis_print);
+
+        // Some debug logging..
+        //console.log(pair_1);
+        //console.log(pair_2);
+        //console.log(item.timestamp+':'+item.place_id);
+        console.log(timestamp_place_pair);
+        console.log(reversed_timestamp_place_pair);
+        
+      });
+    });
+
+    // Update user with this newly visited place.
+    req.redis_client.lpush('u:'+item.user_id, item.place_id, req.redis_print);
+
+    req.redis_client.lrange('u:'+item.user_id, 0, 9, function(err, uu){console.log('-'+item.user_id+'-: '+uu);});
+      
+
+
+
+
+    // Old dummy train code.
+
     // Create dummy data for dummy recommendation.
-    req.redis_client.hincrby("place_counts", item.place_id, 1, req.redis_print);
+    //req.redis_client.hincrby("place_counts", item.place_id, 1, req.redis_print);
     //req.redis_client.zincrby("place_sort", 1, item.place_id, req.redis_print);
+
+
 
 
 
